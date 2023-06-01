b75d3acd9716dc92371fdac6c836275106f18d46
NawazAsghar <nawazasghar.nza@gmail.com>
2023-06-01 11:31:30 +0500

    remove browseable api

--- bebloger/settings.py
+++ bebloger/settings.py
@@ -91,7 +91,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
 
 STATIC_URL = '/static/'
